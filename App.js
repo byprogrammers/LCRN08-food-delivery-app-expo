f7b4f7f50c19a5a5ba1e3b589e419a2a42fd6194
Vincent <booncheng89@gmail.com>
2021-03-02 23:10:40 +0800

    Added Expo version of Food Delivery App

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native'
+import { useFonts } from 'expo-font';
+
+
+
+import { Restaurant, OrderDelivery } from './screens'
+import Tabs from './navigation/tabs'
+
+const Stack = createStackNavigator();
+
+const App = () => {
+
+    const [loaded] = useFonts({
+      "Roboto-Black" : require('./assets/fonts/Roboto-Black.ttf'),
+      "Roboto-Bold" : require('./assets/fonts/Roboto-Bold.ttf'),
+      "Roboto-Regular" : require('./assets/fonts/Roboto-Regular.ttf'),
+
+    })
+    
+    if(!loaded){
+      return null;
+    }
+    
+    
+      return (
+          <NavigationContainer>
+              <Stack.Navigator
+                  screenOptions={{
+                      headerShown: false
+                  }}
+                  initialRouteName={'Home'}
+              >
+                  <Stack.Screen name="Home" component={Tabs} />
+                  <Stack.Screen name="Restaurant" component={Restaurant} />
+                  <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
+              </Stack.Navigator>
+          </NavigationContainer>
+      )
+    
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ constants/maps.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = "<Your Google Maps API Key>"
+
+export default GOOGLE_API_KEY
